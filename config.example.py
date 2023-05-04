45604e0580e9a68dedc1a9cc197e6baf173c89ec
Scrubbs <damonmurdoch1@gmail.com>
2023-05-05 00:51:47 +1000

    Re-added example config

---
+++ config.example.py
@@ -0,0 +1,11 @@
+# Endianness for int parsing
+ENDIANNESS = "little"
+
+# Time (seconds) to wait before next request
+TIMEOUT = 5
+
+# Access Card Filename (Overwritten by CLI)
+FILENAME = "CARD_FILEPATH_GOES_HERE"
+
+# Log file (Overwritten by CLI)
+LOG_FILE = "nfc.log"
